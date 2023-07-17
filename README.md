a9faf6ec17cd6ae118077dae0cf01b70ec2f983c
moking55 <moking55@hotmail.com>
2023-07-18 04:23:08 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,43 @@
+# Maythawat Mahawa (Tawan) - CV Repository
+
+Welcome to my CV repository! This repository serves as a showcase of my skills, projects, and experiences as a computer science student and aspiring programmer. Here you will find information about my educational background, my passion for programming, and the various projects I have worked on.
+
+### About Me
+
+I am Maythawat Mahawa, also known as Tawan. Currently, I am pursuing a degree in computer science at Maejo University. With a strong passion for technology and a natural curiosity for learning, I am constantly seeking new challenges and opportunities to expand my knowledge in the field of programming.
+
+## Projects
+
+1. Blockify - Simple Minecraft Webshop Platform
+
+- Description: A straightforward webshop platform specifically designed for Minecraft enthusiasts.
+
+2. Lanna Words - Simple Word Guessing Game
+
+- Description: An engaging word guessing game with a user-friendly interface.
+
+3. VTuber.in.th - Thai VTuber Tracking Website
+
+- Description: A website dedicated to tracking and providing information on popular virtual YouTubers from Thailand.
+
+4. MC Mart - Standalone Minecraft Webshop
+
+- Description: A fully functional webshop for Minecraft, built using the CodeIgniter 4 framework.
+
+5. ClassX - Online Classroom
+
+- Description: An efficient and user-friendly online classroom platform, facilitating seamless virtual learning experiences.
+
+6. LTC Guide - Student Handbook Application
+
+- Description: A comprehensive student handbook application developed using the Flutter framework.
+
+## Contact Information
+
+If you have any questions, suggestions, or collaboration opportunities, please feel free to reach out to me using the following contact information:
+
+- Name: Maythawat Mahawan
+- Email: tawan.csmju@gmail.com
+- LinkedIn: https://www.linkedin.com/in/maythawat-mahawan-011ba91ba/
+
+Thank you for visiting my CV repository! I look forward to connecting with you and discussing potential opportunities.
---
+++ src/App.vue
@@ -0,0 +1,136 @@
+<template>
+  <div class="resume-header text-light mb-4">
+    <div class="container py-5">
+      <div class="row">
+        <div class="col-6 col-md-3 text-start">
+          <p class="m-0">mju6404101332@mju.ac.th</p>
+          <p class="m-0">tawan.csmju@gmail.com</p>
+          <p class="m-0">Chaing Mai, Thailand</p>
+        </div>
+        <div class="col-12 col-md-6 text-center order-md-0 order-first">
+          <h1>Maythawat Mahawan</h1>
+          <h4 style="color: lightgrey">(Tawan)</h4>
+        </div>
+        <div class="col-6 col-md-3 text-end text-white">
+          <a class="d-block text-light" href="//github.com/moking55">github.com/moking55</a>
+          <a class="d-block text-light" href="//codename-t.com">codename-t.com</a>
+          <a class="d-block text-light" href="//www.linkedin.com/in/maythawat-mahawan-011ba91ba/"
+            >Linkedin</a
+          >
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="container py-4">
+    <div class="row">
+      <div class="col-12">
+        <p>
+          I am a computer science student at Maejo University with a passion for learning and
+          sharing knowledge. I am a quick learner and I am always eager to take on new challenges. I
+          am also a secretary in the computer club, where I help to organize events and promote
+          computer science to other students.
+        </p>
+
+        <p>
+          I am confident that I have the skills and knowledge necessary to be a successful
+          programming intern. I am a hard worker and I am always willing to go the extra mile. I am
+          also a team player and I am able to work well with others. I am eager to learn more about
+          the programming industry and I am confident that I can make a significant contribution to
+          your team.
+        </p>
+        <!-- <p>
+          I am a dedicated and ambitious computer science student currently enrolled at Maejo
+          University. Known for my quick learning abilities and an insatiable appetite for new
+          challenges, I thrive in dynamic environments that allow me to expand my skills and
+          knowledge. One of my greatest passions is sharing knowledge with others, as I believe in
+          the power of collaboration and the positive impact it can have on the growth of
+          individuals and the field as a whole. During my spare time, I immerse myself in creating
+          personal website projects and delving into the intricacies of various web technologies.
+          Additionally, I hold the position of secretary in the computer club, where I actively
+          contribute to the advancement of computer science. With a strong commitment to excellence
+          and a constant drive for self-improvement, I am eagerly seeking a programming internship
+          opportunity to further develop my skills and contribute to real-world projects.
+        </p> -->
+        <!-- <p>
+          My name is Maythawat Mahawan (Tawan) I am a computer science student at Maejo University
+          with a passion for learning and sharing knowledge. I am a quick learner and I am always
+          eager to take on new challenges. In my spare time, I enjoy building my own website
+          projects and learning new web technologies. I am also the secretary of the computer club
+          at Maejo University, where I help to organize events and promote computer science
+          education.
+        </p>
+
+        <p>
+          I am confident that I have the skills and knowledge necessary to be a successful
+          programming intern. I am a hard worker and I am always willing to go the extra mile. I am
+          also a team player and I am able to work well with others. I am eager to learn more about
+          the programming industry and I am confident that I can make a significant contribution to
+          your team.
+        </p> -->
+      </div>
+      <div class="col-12 mt-3">
+        <h4>EDUCATION</h4>
+        <hr />
+        <ul>
+          <li><b>VC. Technical Computer,</b> Lamphun Technical Collage, TH 2018 - 2021</li>
+          <li><b>B. Computer Science,</b> Maejo University, TH 2021 - now</li>
+        </ul>
+      </div>
+      <div class="col-12 mt-3">
+        <h4>TECHNOLOGIES AND LANGUAGES</h4>
+        <hr />
+        <ul>
+          <li>Languages: JavaScript, Node.js (intermediate). TypeScript, SQL, PHP, HTML, CSS.</li>
+          <li>Frameworks: Codeigniter 4, Vue.JS, Vuetify, Bootstrap, Bulma.</li>
+          <li>Other: AWS, Firebase, Vercel, Github, CloudFlare.</li>
+        </ul>
+      </div>
+      <div class="col-12 mt-3">
+        <h4>PROJECTS</h4>
+        <hr />
+        <ul>
+          <li>
+            <b>Blockify -</b> A straightforward webshop platform designed specifically for Minecraft
+            enthusiasts.
+          </li>
+          <li>
+            <b>Lanna Words -</b> An engaging word guessing game with a user-friendly interface.
+          </li>
+          <li>
+            <b>VTuber.in.th -</b> A Thai vtuber tracking website that provides information and
+            updates on popular virtual YouTubers.
+          </li>
+          <li>
+            <b>MC Mart -</b> A standalone Minecraft webshop developed using CodeIgniter 4 framework.
+          </li>
+          <li>
+            <b>ClassX -</b> An efficient online classroom platform for seamless virtual learning
+            experiences.
+          </li>
+          <li>
+            <b>LTC Guide -</b> A handy student handbook application created using the Flutter
+            framework.
+          </li>
+        </ul>
+      </div>
+      <div class="col-12 mt-3">
+        <h4>INTERESTS</h4>
+        <hr />
+        <ul>
+          <li>Areas : Coding, Productivity & Life-hacks</li>
+          <li>Beverage : Americano, Iced Espresso, Matcha tea</li>
+          <li>Games : Arcade, Indie, FPS</li>
+          <li>Pet : I have a cat name Khaijeaw </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+<script lang="ts">
+export default {};
+</script>
+<style>
+.resume-header {
+  background-color: #3f2e3e;
+}
+</style>
