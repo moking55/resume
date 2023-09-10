08d66f84f9e20c19a75c4da1dde00d0436101f00
moking55 <moking55@hotmail.com>
2023-09-11 05:14:31 +0700

    fix index error enable js check

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.min.js";
 import "@/assets/main.css";
 import { createApp } from "vue";
+// @ts-ignore
 import App from "./App.vue";
 
 createApp(App).mount("#app");
